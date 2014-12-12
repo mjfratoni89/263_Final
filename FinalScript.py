990e60ee95ed26d84f29a8bf361b06aa45d6919a
Michael Fratoni <michael.j.fratoni@gmail.com>
2014-12-11 23:32:46 -0800

    found some stuff for correlation, prelim work in final script

--- FinalScript.py
+++ FinalScript.py
@@ -1,3 +1,10 @@
 import numpy as np
+import os
 
-data = np.genfromtxt('leagues_NBA_2014_team.csv')
+os.chdir('Teams')
+for f in os.listdir('/'):
+    #v=os.path.join(f)
+    print (f)
+    for r in os.listdir(v):
+        data = np.genfromtxt(r,delimiter=',',skip_header=1)
+        print(data)
--- team stats correlation.py
+++ team stats correlation.py
@@ -1,6 +1,31 @@
-import os
 import numpy as np
 
-cor_data = np.genfromtxt('leagues_NBA_2014_team.csv', delimiter=',', skip_header=1,usecols=0)
+data = np.genfromtxt('leagues_NBA_2014_team.csv',delimiter=',',skip_header=1,usecols=(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24))
+#1st col was team names (introduced as 'nan')
+#2nd col is wins (now the 1st)
+#I don't think we need the names to correlate, just the wins
 
-print(cor_data)
+# if someone knows how to condense the usecols to ignore 
+# just the first col, do that
+
+cor_matrix=np.corrcoef(data)
+cor_matrix=cor_matrix[0,:]
+
+max_corr=0
+for i in range(len(cor_matrix)):
+    if max_corr<cor_matrix[i] and cor_matrix[i]<1:
+        max_corr=cor_matrix[i]
+max_corr2=0
+for i in range(len(cor_matrix)):
+    if max_corr2<cor_matrix[i] and cor_matrix[i]<max_corr:
+        max_corr2=cor_matrix[i]
+max_corr3=0
+for i in range(len(cor_matrix)):
+    if max_corr3<cor_matrix[i] and cor_matrix[i]<max_corr2:
+        max_corr3=cor_matrix[i]
+
+print(cor_matrix)
+print(max_corr,max_corr2,max_corr3)
+
+indexes=[np.where(cor_matrix==max_corr),np.where(cor_matrix==max_corr2),np.where(cor_matrix==max_corr3)]
+print(indexes)
