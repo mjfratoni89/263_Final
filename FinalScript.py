3c8e92c70eae6edddab6a62b75112e9c96421bdb
Michael Fratoni <michael.j.fratoni@gmail.com>
2014-12-13 16:08:20 -0800

    did something

--- FinalScript.py
+++ FinalScript.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from sklearn import tree
 
 owd=os.getcwd()
 x=0 #see the x below
@@ -7,15 +8,15 @@ for team in os.listdir('./Teams'):
     # v=os.chdir(team)
     if team not in '.ipynb_checkpoints':
         for player in os.listdir('./Teams/' + team):
-            #data = np.genfromtxt(r,delimiter=',',skip_header=2)
-            #x==n
-            #print(r)
             data=[]
             with open('./Teams/' + team + '/' + player,'r') as q:
                 for line in q:
                     l = line.strip().split(',')
+                    for i in range(len(l)):
+                        if l[i]=='':
+                            l[i]=0
                     #all the if statements gets rid of everything thats not yearly stats
-                    if l[0]=='':
+                    if l[0]==0:
                         x==1
                     elif l[0]=='Career':
                         x==1 
@@ -28,9 +29,10 @@ for team in os.listdir('./Teams'):
                     else:
                         #these are the stats we decide to use
                         #here I have the yr, age, team, games played
-                        data.append([str(l[0]),float(l[1]),str(l[2]),float(l[5])])
+                        data.append([str(l[0]),float(l[1]),str(l[2]),float(l[5]),float(l[10])])
             data=np.array(data)
             stats=[]
+            tst_stats=[]
             for i in range(len(data)):
                 if data[i,0].split('-')[0]=='2009':
                     stats.append(data[i,:])
@@ -41,21 +43,31 @@ for team in os.listdir('./Teams'):
                 if data[i,0].split('-')[0]=='2012':
                     stats.append(data[i,:])
                 if data[i,0].split('-')[0]=='2013':
-                    stats.append(data[i,:])                
-                    
+                    stats.append(data[i,:])
+                if data[i,0].split('-')[0]=='2014':
+                    tst_stats.append(data[i,:])                    
             stats=np.array(stats)
-            fstats=[]
-            print(player)
+            tst_stats=np.array(tst_stats)
+    
+            trn_stats=[]
             for j in range(len(stats)):
                 if j>0:
                     if not stats[j,1]==stats[(j-1),1]:
-                        fstats.append(stats[j,:])
+                        trn_stats.append(stats[j,:])
                 else:
-                    fstats.append(stats[j,:])
-            fstats=np.array(fstats)
+                    trn_stats.append(stats[j,:])
+            trn_stats=np.array(trn_stats)
+            if not trn_stats.size==0:
+                trn_stats1=trn_stats[:,1]
+                trn_stats2=trn_stats[:,3]
+                trn_stats3=trn_stats[:,4]
+                trn_stats=np.vstack((trn_stats1,trn_stats2,trn_stats3))
+
+            print(player)                    
+            print(trn_stats)
+            print(tst_stats)
+            dt=tree.DecisionTreeRegressor(max_depth=5)
                     
-            print(fstats)
-        
         
                         
             
